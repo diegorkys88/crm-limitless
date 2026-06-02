c20844fe1a03aa8f4abaf86152a914729e0aced5
diegorkys88 <diegorkys@gmail.com>
2026-06-01 21:26:28 -0400

    Fix API URL and frontend updates

--- static/dashboard.js
+++ static/dashboard.js
@@ -1,4 +1,6 @@
-const API = 'http://127.0.0.1:8000';
+const API = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+  ? 'http://127.0.0.1:8000'
+  : `${window.location.protocol}//${window.location.host}`;
 let allContacts = [];
 let allOutreach = [];
 let allAppointments = [];
